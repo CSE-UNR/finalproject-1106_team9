35350a57b5e4372e6143fb9ac07cde13e348c6cf
1106-grossman-keilor <kgrossman@nevada.unr.edu>
2024-04-30 19:42:47 +0000

    menu function and get file function added, be aware that the get file is not entirely working. also sample image txt added

--- FinalProject.c
+++ FinalProject.c
@@ -5,8 +5,19 @@
 #include <stdio.h>
 
 #define MAXSTRING_LENGTH 50
+#define MAXFILE_SIZE 500
+
+void getFile();
+void getMenu();
 
 int main() {
+
+	getMenu();
+	
+	return 0;
+}
+
+void getMenu(){
 
 	int menuoption;
 	
@@ -15,10 +26,45 @@ int main() {
 	printf("(2): Increase the image brightness\n");
 	printf("(3): Decrease the image brightness\n");
 	printf("(4): Crop current image\n");
-	printf("(5): Save current image\n");
+	printf("(5): Save current image to file\n");
+	printf("(6): Display current image\n");
 	printf("(0): Exit program\n");
 	printf("Please enter the number of the option you would like to choose: ");
 	scanf("%d", &menuoption);
 	
-	return 0;
+	switch(menuoption){
+		case 1:
+			getFile();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 0:
+			return;
+		default:
+			printf("invalid imput, good bye");
+			break;
+	}
+	return;
 }
+
+void getFile(){
+	
+	char File[MAXFILE_SIZE];
+	
+	printf("Please enter the name of the file you would like to edit: ");
+	fgets(File, MAXFILE_SIZE, stdin);
+	
+}
+
+	
+
+
+
